fafe8f95053a4052fcf81f2b341c0734a9f67012
dim <dimensioncoder@outlook.com>
2021-04-28 21:57:57 -0400

    Implement basic screen transition system
    -Create "view" sections for single page app
    -Create screen animations to enter/leave view
    -Add/remove a class to indicate a screen is active/inactive
    -Move animations into different css files for import

--- public/js/test.js
+++ public/js/test.js
@@ -21,8 +21,48 @@ function initBgBoxes(){
     }
 }
 
+function hideScreen(e){
+    let screen = e.currentTarget;
+    screen.classList.remove('screen-active');
+    screen.classList.add('screen-inactive');
+    screen.style.animation = '';
+
+    screen.removeEventListener('animationend', hideScreen);
+}
+
+function anchorScreen(e){
+    let screen = e.currentTarget;
+    screen.classList.remove('screen-inactive');
+    screen.classList.add('screen-active');
+    screen.style.animation = '';
+
+    screen.removeEventListener('animationend', anchorScreen);
+    console.log('anchor called')
+}
+
+function startScreenTransition(e){
+    let splashScreen = document.getElementById('memo-splash-screen');
+    splashScreen.addEventListener('animationend', hideScreen);
+
+    let appScreen = document.getElementById('memo-application-screen');
+    appScreen.addEventListener('animationend', anchorScreen);
+
+    //Animations defined in memo-animations.css
+    splashScreen.style.animation = 'screen-begone 2s ease-in-out 1s 1';
+    appScreen.style.animation = 'screen-bootycall 2s ease-in-out 1s 1';
+
+    console.log('Registered animations');
+}
+
+function initViewScreens(){
+
+    let startButton = document.getElementById('key-submit-btn');
+    startButton.addEventListener('click', startScreenTransition);
+}
+
 function init(){
     //initBgBoxes();
+    initViewScreens();
 }
 
 window.onload = init;
